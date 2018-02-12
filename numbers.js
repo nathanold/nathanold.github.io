0c5d95afd31659f028024529b2c49a79cb370b5f
nathanold <nathanoldakowski@gmail.com>
2018-02-12 15:30:12 -0500

    Homework 3

---
+++ numbers.js
@@ -0,0 +1,14 @@
+// square a number
+function squared(a1){
+    return a1*a1;
+}
+
+// add ten to a number
+function plusTen(a1){
+    return a1+10;
+}
+
+var numbers = {
+    "squared":squared,
+    "plusTen":plusTen
+};
